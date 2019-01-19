748d92c4b6f3a9bd630a3530d771c52b38161b27
qiubeiping8 <beipingq@gmail.com>
2019-01-19 12:47:18 -0500

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Main } from "./Main";
+
+import { TopNavBar } from './TopNavBar';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <TopNavBar />
+          <Main />
+      </div>
+    );
+  }
+}
+
+export default App;
